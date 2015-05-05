f610ba20dff6d4cbff52100e2928e770c3281123
Joseph Mizrahi <jomizrahi92@yahoo.com>
2015-05-05 19:01:16 -0400

    Created layout

---
+++ main.js
@@ -0,0 +1,12 @@
+function startCalender() {
+	var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']; var weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+	var dateObject = new Date();
+	var todaysDate = dateObject.getDate();
+	var sDateObject = new Date();
+	sDateObject.setTime(dateObject.getTime());
+	var mDateObject = new Date();
+	mDateObject.setTime(dateObject.getTime());
+}
+
+function buildCalender(){
+}
