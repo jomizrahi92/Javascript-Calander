5eb9c774590a380e2067eeac039fb7e1f13b144d
Joseph Mizrahi <jomizrahi92@yahoo.com>
2015-05-05 23:07:10 -0400

    Todays date

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
-function startCalender() {
-	var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']; var weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-	var dateObject = new Date();
-	var todaysDate = dateObject.getDate();
-	var sDateObject = new Date();
-	sDateObject.setTime(dateObject.getTime());
-	var mDateObject = new Date();
-	mDateObject.setTime(dateObject.getTime());
-}
+window.onload = function(){
+	startCalender();
 
-function buildCalender(){
+	function startCalender() {
+		var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']; 
+		var weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+		var dateObject = new Date();
+		var todaysDate = dateObject.getDate();
+		var sDateObject = new Date();
+		sDateObject.setTime(dateObject.getTime());
+		var mDateObject = new Date();
+		mDateObject.setTime(dateObject.getTime());
+
+		buildCalender();
+
+		function buildCalender(){
+		var date = document.querySelector('#currentDay p');
+		date.innerHTML += weekdays[dateObject.getDay()];
+
+		var currentDate = document.querySelector('#currentDate p');
+		currentDate.innerHTML += dateObject.getDate();
+		}
+	}
 }
