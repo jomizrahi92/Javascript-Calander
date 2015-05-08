b085ab70cecfced33848ccdc2c1160c35ac7342d
Joseph Mizrahi <jomizrahi92@yahoo.com>
2015-05-08 19:02:47 -0400

    Got dates working

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-window.onload = function(){
+window.onload = function() {
 	startCalender();
 
 	function startCalender() {
-		var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']; 
+		var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 		var weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 		var weekdaysSmall = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
 		var dateObject = new Date();
@@ -14,69 +14,79 @@ window.onload = function(){
 
 		buildCalender();
 
-		function buildCalender() {
-			var date = document.querySelector('#currentDay p');
-			date.innerHTML += weekdays[dateObject.getDay()];
+		function buildCalender() {				
+				var date = document.querySelector('#currentDay p');
+				date.innerHTML += weekdays[dateObject.getDay()];
 
-			var currentDate = document.querySelector('#currentDate p');
-			currentDate.innerHTML += dateObject.getDate();
-			
-			var currentMonth = document.querySelector('#currentMonth');
-		    currentMonth.innerHTML += months[mDateObject.getMonth()] + ' ' + mDateObject.getFullYear();
+				var currentDate = document.querySelector('#currentDate p');
+				currentDate.innerHTML += dateObject.getDate();
 
-			for (x in weekdaysSmall){
-		    	var daysOfWeek = document.querySelector('.day tr');
-				daysOfWeek.innerHTML += '<th>' + weekdaysSmall[x] + '</th>';
-			}
-			
-			var dateCounter = 1;
-		    mDateObject.setDate(dateCounter);
-		    var currentMonthIndex = mDateObject.getMonth();
-			var dates = document.querySelector('.monthDate');
-			
-			for (var i = 0; i < 6; i++) {
-		    	dates.innerHTML += '<tr>';
-				for (var x = 0; x < 7; x++) {
-		          	dates.innerHTML += '<td';
-					if ((mDateObject.getDate() == dateObject.getDate()) && (mDateObject.getMonth() == dateObject.getMonth()) &&									(mDateObject.getFullYear() == dateObject.getFullYear())) {
-						dates.innerHTML += ' id="today"';
+				var currentMonth = document.querySelector('#currentMonth');
+				currentMonth.innerHTML += months[mDateObject.getMonth()] + ' ' + mDateObject.getFullYear();
+
+				for (x in weekdaysSmall) {
+					var daysOfWeek = document.querySelector('.day tr');
+					daysOfWeek.innerHTML += '<th>' + weekdaysSmall[x] + '</th>';
+				}
+
+				var dateCounter = 1;
+				mDateObject.setDate(dateCounter);
+				var currentMonthIndex = mDateObject.getMonth();
+				var dates = document.querySelector('.monthDate');
+				
+				var datesString = "";
+
+				for (var i = 0; i < 6; i++) {
+					datesString += '<tr>';
+					
+					for (var x = 0; x < 7; x++) {
+						datesString += '<td';
+						
+						if ((mDateObject.getDate() == dateObject.getDate()) && (mDateObject.getMonth() == dateObject.getMonth()) && (mDateObject.getFullYear() == dateObject.getFullYear())) {
+							datesString += ' id="today"';
+						}
+						
+						datesString += '>';
+
+						if ((mDateObject.getDay() == x) && (mDateObject.getMonth() == currentMonthIndex)) {
+							datesString += mDateObject.getDate();
+							dateCounter++;
+							mDateObject.setDate(dateCounter);
+						} else {
+							datesString += '&nbsp;'
+						}
+						
+						datesString += '</td>';
 					}
-					dates.innerHTML += '>';
-		  
-		          	if ((mDateObject.getDay() == x) && (mDateObject.getMonth() == currentMonthIndex)) {
-			            dates.innerHTML += mDateObject.getDate();
-			            dateCounter++;
-		            	mDateObject.setDate(dateCounter);
-		            } 
-					else {
-			            dates.innerHTML += '&nbsp;'
-		          	}
-		          	dates.innerHTML += '</td>';
-		        }
-		        dates.innerHTML +=  '</tr>';
+					
+					datesString += '</tr>';
+				}
+				
+				if (mDateObject.getMonth() != currentMonthIndex) {
+					mDateObject.setMonth(mDateObject.getMonth() - 1);
+				}
+				
+				datesString += '</table>';
+				
+				dates.innerHTML = datesString;
 			}
-			if (mDateObject.getMonth() != currentMonthIndex) {
-				mDateObject.setMonth(mDateObject.getMonth() - 1);
-		    }
-		    dates.innerHTML += '</table>';
-		}
-		//window.onkeydown = parseKeyboardInput;
+			
+			//window.onkeydown = parseKeyboardInput;
 		function parseKeyboardInput(e) {
 			if (e.keyCode == '37') {
 				prevMonth();
-		    } 
-			else if (e.keyCode == '39') {        
-		        nextMonth();
-		    }
+			} else if (e.keyCode == '39') {
+				nextMonth();
+			}
 		}
 
 		/*function prevMonth() {
 			mDateObject.setMonth(mDateObject.getMonth() - 1);
-		    buildCalendar();
+			buildCalendar();
 		}
 		function nextMonth() {
 			mDateObject.setMonth(mDateObject.getMonth() + 1);
-		    buildCalendar();
+			buildCalendar();
 		}
 		function dateClick(evt) {
 			sDateObject.setDate(evt.target.innerHTML);
